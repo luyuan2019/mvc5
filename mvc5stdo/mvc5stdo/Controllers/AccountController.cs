90a263bea2f0b3815b2ea5dfd23d480aef69c59f
ly <lu_yuan2007@126.com>
2019-02-15 14:38:51 +0800

    no message

--- mvc5stdo/mvc5stdo/Controllers/AccountController.cs
+++ mvc5stdo/mvc5stdo/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using mvc5stdo.Models;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -47,6 +49,48 @@ namespace mvc5stdo.Controllers
 
             return Json(stuModel, JsonRequestBehavior.AllowGet);
         }
+
+
+        [HttpPost]
+        public ActionResult SavePicture(string picString, string id)
+        {
+            var tmpArr = picString.Split(',');
+            byte[] bytes = Convert.FromBase64String(tmpArr[1]);
+            MemoryStream ms = new MemoryStream(bytes);
+            ms.Write(bytes, 0, bytes.Length);
+            var img = Image.FromStream(ms, true);
+
+            var path = System.AppDomain.CurrentDomain.BaseDirectory;
+            var imagesPath = System.IO.Path.Combine(path, @"Uploads\AgrCorporations\" + id + "\\");
+            if (!System.IO.Directory.Exists(imagesPath))
+                System.IO.Directory.CreateDirectory(imagesPath);
+            string fileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", System.Globalization.DateTimeFormatInfo.InvariantInfo) + ".png";
+            var bitImage = GetThumbnailImage(img, 400, 300);
+            bitImage.Save(imagesPath + fileName);
+            bool isOk = true;
+            string msg = imagesPath + fileName;
+            return Json(new { suc = isOk, msg = msg });
+        }
+
+        //图片压缩
+        public static Image GetThumbnailImage(Image srcImage, int width, int height)
+        {
+            Image bitmap = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            //设置高质量插值法 
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+
+            //在指定位置并且按指定大小绘制原图片的指定部分 
+            g.DrawImage(srcImage, new Rectangle(0, 0, width, width),
+                new Rectangle(0, 0, srcImage.Width, srcImage.Height),
+                GraphicsUnit.Pixel);
+
+            return bitmap;
+        }
+
+
 
 	}
 }
